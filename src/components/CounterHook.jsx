f7694a57f0c0cc1097d343e397086765bb5fee14
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-08-09 09:22:58 +0300

    class vs hook

--- src/components/CounterHook.jsx
+++ src/components/CounterHook.jsx
@@ -4,14 +4,15 @@ function CounterHook() {
 
 
   const arr = useState(0);
-  console.log(arr);
+  // console.log(arr);
   const counter = arr[0]
+  const setCounter = arr[1]
 
   return ( <div>
     <h2>Counter</h2>
     <h2>{counter}</h2>
     <button>-</button>
-    <button>+</button>
+    <button onClick={() => setCounter(counter+1)}>+</button>
   </div> );
 }
  
