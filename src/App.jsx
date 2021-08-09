0872efef1e7a10374de099665acdc56e921f8f0d
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-08-09 09:50:40 +0300

    useEffect changes title

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css';
-import Counter from './components/Counter';
+// import Counter from './components/Counter';
 import CounterHook from './components/CounterHook';
 
 function App() {
   return (
     <div className="App container">
-      <Counter/>
-      <CounterHook/>
+      {/* <Counter/> */}
+      <CounterHook />
     </div>
   );
 }
--- src/components/CounterHook.jsx
+++ src/components/CounterHook.jsx
@@ -1,14 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function CounterHook() {
   const [counter, setCounter] = useState(0);
   const [name, setName] = useState('');
 
+  // componentdidmouny
+  // componentdidupdate
+  // useEffect() vietoj dvieju virsuj
+  useEffect(() => {
+    console.log('useEffect()');
+    document.title = `${name} has clicked + ${counter}`;
+  });
+
   return (
     <div>
       <h2>Counter of {name}</h2>
       <h2>
-        {name} has clicked {counter} times
+        {name} has clicked + {counter} times
       </h2>
       <button onClick={() => setCounter(counter + 1)}>+</button>
       <button onClick={() => setName('James')}>set name to James</button>
