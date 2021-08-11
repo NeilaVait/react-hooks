1f21c0f7648778f98ace35e22905d525b3d86d43
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-08-11 13:53:35 +0300

    app to class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import React, { Component } from 'react';
 import './App.css';
 // import Counter from './components/Counter';
 // import CounterHook from './components/CounterHook';
 import MoviePage from './context/MoviePage';
 
-function App() {
-  return (
-    <div className="App container">
-      {/* <CounterHook /> */}
-      <MoviePage />
-    </div>
-  );
+class App extends Component {
+  state = {
+    userName: 'james',
+  };
+
+  render() {
+    return (
+      <div className="App container">
+        {/* <CounterHook /> */}
+        <MoviePage />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/context/MoviePage.jsx
+++ src/context/MoviePage.jsx
@@ -3,6 +3,7 @@ import MovieList from './MovieList';
 
 class MoviePage extends Component {
   state = {};
+
   render() {
     return (
       <div>
