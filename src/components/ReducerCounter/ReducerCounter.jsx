22d334365c1617bff213e63243b89de1e780c51a
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-08-18 08:50:25 +0300

    reducer + - reset works

--- src/components/ReducerCounter/ReducerCounter.jsx
+++ src/components/ReducerCounter/ReducerCounter.jsx
@@ -1,22 +1,41 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+function countReducer(state, action) {
+  if (action.type === 'increment') {
+    return { count: state.count + 1 };
+  }
+  if (action.type === 'decrement') {
+    return { count: state.count - 1 };
+  }
+  if (action.type === 'reset') {
+    return { count: 0 };
+  }
+}
 
 function ReducerCounter() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(countReducer, { count: 0 });
 
   const increment = () => {
-    setCount(count + 1);
+    dispatch({ type: 'increment' });
   };
 
   const decrement = () => {
-    setCount(count - 1);
+    // setCount(count - 1);
+    dispatch({ type: 'decrement' });
+  };
+
+  const resetCounter = () => {
+    dispatch({ type: 'reset' });
   };
 
   return (
     <div>
       <h4>ReducerCounter</h4>
-      <h2>{count}</h2>
+      <h2>{state.count}</h2>
       <button onClick={increment}>+</button>
       <button onClick={decrement}>-</button>
+      <button onClick={resetCounter}>Reset</button>
     </div>
   );
 }
